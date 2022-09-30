1cf10f7a59b206ad17b188f0686d91ec1eabe063
konstantinosrizos <rizos.dev@gmail.com>
2022-09-30 12:39:01 +0100

    TicketView added

---
+++ MovieBooking/MovieBooking/Views/TicketView.swift
@@ -0,0 +1,20 @@
+//
+//  TicketView.swift
+//  MovieBooking
+//
+//  Created by Konstantinos Rizos on 30/9/22.
+//
+
+import SwiftUI
+
+struct TicketView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TicketView_Previews: PreviewProvider {
+    static var previews: some View {
+        TicketView()
+    }
+}
