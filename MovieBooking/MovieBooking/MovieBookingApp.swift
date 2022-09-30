5a1fee41be4bd6c7faf068f45b17e8b0a78ae418
konstantinosrizos <rizos.dev@gmail.com>
2022-09-30 12:26:41 +0100

    initial stracture of the project

---
+++ MovieBooking/MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingApp.swift
+//  MovieBooking
+//
+//  Created by Konstantinos Rizos on 30/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
