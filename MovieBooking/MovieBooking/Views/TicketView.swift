cb176fb1e92cc615aab2a3fef04a4e4fd0845fdc
konstantinosrizos <rizos.dev@gmail.com>
2022-10-23 10:59:42 +0100

    Animated background has been added

--- MovieBooking/MovieBooking/Views/TicketView.swift
+++ MovieBooking/MovieBooking/Views/TicketView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct TicketView: View {
+	@State var animate = false
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		ZStack {
+			CircleBackground(color: Color("greenCircle"))
+				.blur(radius: animate ? 30 : 100)
+				.offset(x: animate ? -50 : -130, y: animate ? -30 : -100)
+				.task {
+					withAnimation(.easeInOut(duration: 7).repeatForever()) {
+						animate.toggle()
+					}
+				}
+			
+			CircleBackground(color: Color("pinkCircle"))
+				.blur(radius: animate ? 30 : 100)
+				.offset(x: animate ? 100 : 130, y: animate ? 150 : 100)
+			
+			VStack(spacing: 30) {
+				Text("Mobile Ticket")
+					.font(.title3)
+					.foregroundColor(.white)
+					.fontWeight(.bold)
+				
+				Text("Once you buy a movie ticket simply scan the barcode to access to your movie")
+					.frame(maxWidth: 248)
+					.font(.body)
+					.foregroundColor(.white)
+					.multilineTextAlignment(.center)
+			}
+			.padding(.horizontal, 20)
+			.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+		}
+		.background(LinearGradient(gradient: Gradient(colors: [Color("backgroundColor"), Color("backgroundColor2")]), startPoint: .top, endPoint: .bottom))
     }
 }
 
