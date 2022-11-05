0551f412e4e50aaf74b879c049e8b667c7315139
konstantinosrizos <rizos.dev@gmail.com>
2022-11-05 11:31:14 +0000

    Custom TabBar has been added

---
+++ MovieBooking/MovieBooking/Data/TabData.swift
@@ -0,0 +1,16 @@
+//
+//  TabData.swift
+//  MovieBooking
+//
+//  Created by Konstantinos Rizos on 5/11/22.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable {
+	case home = "Home"
+	case location = "Location"
+	case ticket = "Ticket"
+	case category = "Category"
+	case profile = "Profile"
+}
