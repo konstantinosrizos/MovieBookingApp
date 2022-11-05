828a72fcb1de883b4ba1df93bec14457fbca84b7
konstantinosrizos <rizos.dev@gmail.com>
2022-11-05 11:43:18 +0000

    Custom SearchBar has been added

---
+++ MovieBooking/MovieBooking/Views/HomeView.swift
@@ -0,0 +1,46 @@
+//
+//  HomeView.swift
+//  MovieBooking
+//
+//  Created by Konstantinos Rizos on 5/11/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+	@State var animate = false
+	
+    var body: some View {
+		ZStack {
+			CircleBackground(color: Color("greenCircle"))
+				.blur(radius: animate ? 30 : 100)
+				.offset(x: animate ? -50 : -130, y: animate ? -30 : -100)
+				.task {
+					withAnimation(.easeInOut(duration: 7).repeatForever()) {
+						animate.toggle()
+					}
+				}
+			
+			CircleBackground(color: Color("pinkCircle"))
+				.blur(radius: animate ? 30 : 100)
+				.offset(x: animate ? 100 : 130, y: animate ? 150 : 100)
+			VStack(spacing: 0) {
+				Text("Choose Movie")
+					.fontWeight(.bold)
+					.font(.title3)
+					.foregroundColor(.white)
+				
+				CustomSearchBar()
+					.padding(EdgeInsets(top: 30, leading: 20, bottom: 20, trailing: 20))
+			}
+			.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+		}
+		.background(LinearGradient(colors: [Color("backgroundColor"), Color("backgroundColor2")], startPoint: .top, endPoint: .bottom))
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
